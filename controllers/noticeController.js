daffb313dea159b1c4961af83134d4fad4e55cc0
xmflr95 <xmflr95@gmail.com>
2019-12-09 19:56:54 +0900

    Test

--- controllers/noticeController.js
+++ controllers/noticeController.js
@@ -3,14 +3,15 @@ const Notice = require('../models/Notice');
 
 exports.home = async function(req, res) {
   let notice = new Notice();
+  
   let noticeData = await notice.getNotice();
   let scholarData = await notice.getScholar();
   let employData = await notice.getEmploy();
   
-  res.render('index', {
-    notices: noticeData,
-    scholars: scholarData,
-    employs: employData,
-  });  
+  // res.render('index', {
+  //   notices: noticeData,
+  //   scholars: scholarData,
+  //   employs: employData,
+  // });  
 };
 
